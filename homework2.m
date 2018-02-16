5d9bb82be87d72ae70ce10a14e468ef0a93191ff
Ryan Plante <ryan.plante@maine.edu>
2018-02-16 03:32:37 -0500

    finished hw3

--- homework2.m
+++ homework2.m
@@ -13,54 +13,159 @@
 a1 = [1 -1.5371 0.9025];
 b1 = [1 1.6180 1];
 
-%frequency array, ~1000 point resolution
-f = -0.5:.001:0.5;
+%frequency array, ~10000 point resolution
+f = -0.5:.0001:0.5;
 
 z = exp(i*2*pi*f); %input
 
-%Evaluate H(z) = Y(z)/X(Z) at all points of Z
+%Evaluate H(z) = Y(z)/X(z) at all points of Z
 hz1 = polyval(b1, z) ./polyval(a1, z);
 hz1db = 20*log10(hz1); %convert from magnitude to db
+subplot(1,2,1);
 plot(f, hz1db)
+xlabel('Frequency (Hz)');
+ylabel('Magnitude (db)')
+title('Filter #1');
 
 %Filter 2
-
 a2 = [1 0 -0.81];
 b2 = [1 -0.618 1];
 
 hz2 = polyval(b2, z) ./polyval(a2, z);
 hz2db = 20*log10(hz2);
-figure(2)
+subplot(1,2,2);
 plot(f, hz2db)
-
-
+xlabel('Frequency (Hz)');
+ylabel('Magnitude (db)')
+title('Filter #2');
 
 
 %% Question 4
 %a
 casc =(hz1db+hz2db); %cascade the two filters together
-gain = 1/(20*log10(abs(max(casc)))) %find gain for max 1 db gain
+gain = 1/abs(max(casc)) %find value needed for max 1db gain
 
 %b
-cascadeWithGain = gain*casc;
-
-figure(3)
+cascadeWithGain = gain*casc; %apply our gain
+figure(2)
 plot(f,cascadeWithGain)
-
+xlabel('Frequency (Hz)');
+ylabel('Magnitude (db)');
+title('Cascaded Filter');
 %c
-x = [0 0 0 0 0 0 0 0 3 1.5 -1.8 0.2 0 0 0 0 0 0 0 0];
-stage1 = filter(b1, a1, x*gain)
-stage2 = filter(b2, a2, stage1)
-
+x = [0 0 0 0 0 0 0 0 3 1.5 -1.8 0.2 0 0 0 0 0 0 0 0]; %input array of 20 values
+stage1 = filter(b1, a1, x*gain) %output of stage one
+stage2 = filter(b2, a2, stage1) %output of stage two
 %% Question 5
+%See paper for derivation of coefficients
 a = [1 -1.8*cos(2*pi*(0.3)) 0.81];
 b = [1 -2*cos(2*pi*(0.06)) 1];
 
+%Evaluate H(z) = Y(z)/X(z) at all points of Z
 hz3 = polyval(b, z) ./ polyval(a, z);
-hz3 = 20*log10(hz3);
-figure(11)
+gain2 = 1./abs(max(hz3)) %gain of 0 db
+hz3 = 20*log10(hz3*gain2); %convert to db, add in gain
+figure(3)
 plot(f, hz3)
+xlabel('Frequency (Hz)');
+ylabel('Magnitude (db)');
+title('Filter #3 Design');
 %% Question 6
+clear
+%% A
+M = 4;
+sigma = 1.3;
+G = 1; %placeholder value
 
+%ii
+%compute sum of impulse response to find DC gain
+sum = 0;
+for n = -M:M
+  sum = sum + exp(-0.5*(n/sigma)^2);
+end
+
+G = 1/sum %gain coefficient for DC gain of 1
+
+%iii
+f= (-0.5:0.01:0.5); %frequency, 100 samples
+window = (-M:1:M); %window in which filter is non-zero
+
+%define our filter symbolically
+syms z freq n
+h(n) = piecewise((n >= -M) & (n <= M), G*exp(-0.5*(n/sigma)^2),0); 
+H(z) = 0*z;
+for i = -M:M
+  H(z) = H(z) + h(i)*(z^-i);
+end
+
+%substitute our exponential in for z
+H(freq) = subs(H(z), z, exp(1j*2*pi*freq));
+%plot our filter gain
+figure(4)
+plot(f, abs(double(H(f))))
+xlabel('Frequency (Hz)');
+ylabel('Magnitude')
+title('Gaussian Blur Filter Gain');
+
+%iv
+% Load the image, and convert to doubles for processing.
+p1 = double(imread('Cavvy_bw.jpg'));
+
+%calculate number of rows in image array
+rows = size(p1(:,1));
+
+%for all rows in image create new image processed through gaussian blur
+%filter
+for i= 1:rows
+  p2(i,:) = conv(p1(i,:), double(h(window)));
+end
+figure(5)
+%orignal image
+imshow(p1,[0 255])
+title('Original Image');
+
+figure(6)
+%gaussian blur processed image
+imshow(p2,[0 255])
+title('Gaussian Blur Filtered Image');
+%% B
+%define our edge detection filter
+h2(n) = piecewise((n >= -M) & (n <= M), -G*(n/(sigma^2))*exp(-0.5*((n/sigma)^2)),0);
+
+%i
+%once again find our filter gain
+sum = 0;
+for i = -M:M
+  sum = sum + (i/(sigma^2))*exp(-0.5*((i/sigma)^2));
+end
+%create new gain coefficient
+G = -1/sum
+
+H(z) = 0*z; 
+for i= -M:M
+  H(z) = H(z) + h2(i)*(z^-i);
+end
+
+%substitue our exponential in for z
+H(freq) = subs(H(z), z, exp(1j*2*pi*freq));
+
+%plot filter gain vs frequency
+figure(7)
+plot(f, abs(double(H(f))))
+xlabel('Frequency (Hz)');
+ylabel('Magnitude')
+title('Edge Detection Filter Gain');
+
+%ii
+%for all rows in image create new image processed through edge detection
+%filter
+for i= 1:rows
+  p3(i,:) = conv(p1(i,:), double(h2(window)));
+end
+
+figure(8)
+%edge detection
+imshow(p3, [-20 20])
+title('Edge Detection Filtered Image');
 %% Question 7
-%Mostly on paper, some matlab
+%See paper
--- test.m
+++ test.m
@@ -1,93 +1,95 @@
 %% Problem 6
-clearvars -except fig
-
 %% A
-sigma = 1.3;
 M = 4;
+sigma = 1.3;
+G = 1; %placeholder value
 
-% ii.
+%ii
+%compute sum of impulse response to find DC gain
 sum = 0;
-
-%sum of impulse responses = dc gain
-for i=-M:M
-  sum = sum + exp(-0.5*(i/sigma)^2);
+for n = -M:M
+  sum = sum + exp(-0.5*(n/sigma)^2);
 end
 
-G = 1/sum 
+G = 1/sum %gain coefficient for DC gain of 1
 
-% iii.
-fwindow=[-0.5:0.01:0.5]; %frequency, 100 samples
-nwindow = [-M:1:M]; %window in which filter is applied
+%iii
+f= (-0.5:0.01:0.5); %frequency, 100 samples
+window = (-M:1:M); %window in which filter is non-zero
 
-%define the filter
-syms n f z
-h(n) = piecewise(n>=-M & n<=M, G*exp(-0.5*(n/sigma)^2),0); 
-
-% x[n] z^-n from n = -inf to inf
-H(z) = 0*z; 
-
-%idk what this does right here, symbolic toolbox stuff
-for i=-M:M
+%define our filter symbolically
+syms z freq n
+h(n) = piecewise((n >= -M) & (n <= M), G*exp(-0.5*(n/sigma)^2),0); 
+H(z) = 0*z;
+for i = -M:M
   H(z) = H(z) + h(i)*(z^-i);
 end
 
-H(f) = subs(H(z),z,exp(1j*2*pi*f));
-%weird way to plot impulse reponse
+%substitute our exponential in for z
+H(freq) = subs(H(z), z, exp(1j*2*pi*freq));
+%plot our filter gain
 figure(1)
-plot(fwindow,abs(double(H(fwindow))));
+plot(f, abs(double(H(f))))
+xlabel('Frequency (Hz)');
+ylabel('Magnitude')
+title('Gaussian Blur Filter Gain');
 
-legend('6A');
-% iv.
+%iv
 % Load the image, and convert to doubles for processing.
 p1 = double(imread('Cavvy_bw.jpg'));
 
-%amount of rows in image
-rows = size(p1);
-rows = rows(1);
+%calculate number of rows in image array
+rows = size(p1(:,1));
 
-%for all rows in image create new image processed through impulse response
-for i=1:rows
-  p2(i,:) = conv(p1(i,:),double(h(nwindow)));
+%for all rows in image create new image processed through gaussian blur
+%filter
+for i= 1:rows
+  p2(i,:) = conv(p1(i,:), double(h(window)));
 end
+figure(2)
+%orignal image
+imshow(p1,[0 255])
+title('Original Image');
 
+figure(3)
+%gaussian blur processed image
+imshow(p2,[0 255])
+title('Gaussian Blur Filtered Image');
 %% B
-%new impulse response
-h2(n) = piecewise(n>=-M & n<=M, -G*(n/(sigma^2))*exp(-0.5*((n/sigma)^2)),0);
+%define our edge detection filter
+h2(n) = piecewise((n >= -M) & (n <= M), -G*(n/(sigma^2))*exp(-0.5*((n/sigma)^2)),0);
 
-%plotting filter gain again
+%i
+%once again find our filter gain
 sum = 0;
-for n=-M:M
-  sum = sum + (n/(sigma^2))*exp(-0.5*((n/sigma)^2));
+for i = -M:M
+  sum = sum + (i/(sigma^2))*exp(-0.5*((i/sigma)^2));
 end
-
+%create new gain coefficient
 G = -1/sum
 
-% x[n] z^-n from n = -inf to inf
 H(z) = 0*z; 
-for i=-M:M
+for i= -M:M
   H(z) = H(z) + h2(i)*(z^-i);
 end
 
-H(f) = subs(H(z),z,exp(1j*2*pi*f));
+%substitue our exponential in for z
+H(freq) = subs(H(z), z, exp(1j*2*pi*freq));
 
-figure(2)
-plot(fwindow,abs(double(H(fwindow))));
-
-legend('6B');
-
-%image detection on image
-for i=1:rows
-  p3(i,:) = conv(p1(i,:),double(h2(nwindow)));
-end
-
-figure(3)
-%orignal image
-imshow(p1,[0 255]);
-
+%plot filter gain vs frequency
 figure(4)
-%impulse response
-imshow(p2,[0 255]);
+plot(f, abs(double(H(f))))
+xlabel('Frequency (Hz)');
+ylabel('Magnitude')
+title('Edge Detection Filter Gain');
+
+%for all rows in image create new image processed through edge detection
+%filter
+for i= 1:rows
+  p3(i,:) = conv(p1(i,:), double(h2(window)));
+end
 
 figure(5)
 %edge detection
-imshow(p3,[-20 20]);
+imshow(p3, [-20 20])
+title('Edge Detection Filtered Image');
